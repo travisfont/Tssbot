9499e234761876a123a4c519b80be03c6887ecb4
Travis van der F. <travis.font@gmail.com>
2013-10-31 21:29:40 -0400

    Update tssbot.php

--- tssbot.php
+++ tssbot.php
@@ -4,14 +4,14 @@ set_time_limit(0);
 
 /* configs */
 
-define('HOST',		'localhost');
-define('USER',      'root');
-define('PASSWORD',  NULL);
-define('DATABASE', 	'tssbot');
+define('HOST',     'localhost');
+define('USER',     'root');
+define('PASSWORD', NULL);
+define('DATABASE', 'tssbot');
 
-define('SERVER',    'pratchett.freenode.net');
-define('PORT',      6667);
-define('NICK', 		'Tssbot'.rand());
+define('SERVER',   'pratchett.freenode.net');
+define('PORT',     6667);
+define('NICK',     'Tssbot'.rand());
 
 // the great beloved channel list
 $channels = array
